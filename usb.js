58ed11c94037bd7d7f30d31616e7da1f7a53005f
Martin Endler <pokusew@seznam.cz>
2017-04-08 07:33:18 +0200

    Test

--- usb.js
+++ usb.js
@@ -1,6 +1,6 @@
 var binary = require('node-pre-gyp');
 var path = require('path');
-var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
+var binding_path = './src/binding/usb_bindings.node';
 
 var usb = exports = module.exports = require(binding_path);
 var events = require('events')
